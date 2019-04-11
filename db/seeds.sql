922ee0cfbcea7ea715eada689226adbd8520e716
KTurnage <kristaturnage314@gmail.com>
2019-04-11 09:42:50 -0600

    almost working

---
+++ db/seeds.sql
@@ -0,0 +1,5 @@
+INSERT INTO burgers (name) VALUES ('Cheese Burger');
+INSERT INTO burgers (name) VALUES ('Veggie Burger');
+INSERT INTO burgers (name, eaten) VALUES ('Bacon Burger', true);
+INSERT INTO burgers (name, eaten) VALUES ('Monster Burger', true);
+
